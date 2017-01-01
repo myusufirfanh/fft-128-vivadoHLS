8d6a69a6e54a08d2de2bc24c3905c09000ce6700
M Yusuf Irfan H <koboli@outlook.com>
2017-01-01 19:18:30 +0700

    Add files via upload

---
+++ fft.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void FFT( double c[256], int N, int isign);
+
+void fft_01();
---
+++ testbench.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fft.h"
+
+int main () {
+	fft_01();
+	return 0;
+}
+
